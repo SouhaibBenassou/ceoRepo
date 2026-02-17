c0dd7d6153515d8c317776bfe89b3b1244c6f6ae
Souby <Benassou.sohaib@gmail.com>
2026-02-17 16:39:53 +0000

    add testimonails and video introtions

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,19 @@ import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
 import CreatorsSection from "@/components/creators"
 import { BackGroundLight } from "@/components/background-light"
+import { VideoPlayer } from "@/components/video-player"
+import { SliderTestimonails } from "@/components/slider-testimonials"
+
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black"> 
+    <main className="min-h-screen bg-[#020508]"> 
      <BackGroundLight />
       <Header />
       <Hero />
-      <CreatorsSection/>
+      <CreatorsSection/> 
+      <VideoPlayer/>
+      <SliderTestimonails/>
     </main>
   )
 }
